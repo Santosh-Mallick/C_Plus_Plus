8eaacf3ceee431e54d1548aa6a7649716a467a76
Santosh-Mallick <sant6225@gmail.com>
2024-09-14 23:59:16 +0530

    operatorsandwhileloop

---
+++ DSA/lc10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char name = 'b';
+
+    if (name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+    cout<<"vowel";
+    else
+    cout<<"consonant"<<endl;
+    cout<<!5; //return 0
+    return 0;
+}
---
+++ DSA/lc11.cpp
@@ -0,0 +1,40 @@
+// Multiple of 8
+// #include<iostream>
+// using namespace std;
+
+// int main() { 
+//     int n=1,i=8;
+//     while (n<=10)
+//     {
+//         cout<<i*n<<endl;
+//         n++;
+//     }
+//     return 0;
+// }
+// Factors of 10
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int i=1;
+//     while(i<=10){
+//         if(10%i==0){
+//         cout<<i<<endl;
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+// Even Or Odd
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int i=1;
+//     while(i<=20){
+//         if (i%2==0){
+//             cout<<"Even :"<<i<<endl;
+//         } else {
+//             cout<<"Odd :"<<i<<endl;
+//         }
+//         i++;
+//     }
+// }
